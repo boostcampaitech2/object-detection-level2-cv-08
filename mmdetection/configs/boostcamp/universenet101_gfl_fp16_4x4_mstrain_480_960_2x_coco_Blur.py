d4c7d054224a71e79414cdc8a8d2d2716f5a7954
gunwooHan <gunwoohan@naver.com>
2021-10-12 17:28:49 +0900

    EDIT universenet101 configs

--- mmdetection/configs/boostcamp/universenet101_gfl_fp16_4x4_mstrain_480_960_2x_coco_Blur.py
+++ mmdetection/configs/boostcamp/universenet101_gfl_fp16_4x4_mstrain_480_960_2x_coco_Blur.py
@@ -4,39 +4,17 @@ _base_ = [
     'models/universenet101_gfl.py',
     'config/dataset_mstrain_480_960_Blur.py',
     'config/default_runtime.py',
-    'config/schedule.py'
+    'config/schedule_adam.py'
 ]
 model = dict(
         init_cfg=dict(type='Pretrained', checkpoint=pretrained),
 )
-
-
-data = dict(samples_per_gpu=4)
-
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(
-    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
-lr_config = dict(warmup_iters=1000)
-
 fp16 = dict(loss_scale=512.)
-
-# optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=None)
-# learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[16, 22])
-runner = dict(type='EpochBasedRunner', max_epochs=24)
-
 log_config = dict(
     _delete_=True,
     interval=50,
     hooks=[
         dict(type="TextLoggerHook"),
-        dict(type="WandbLoggerHook", init_kwargs=dict(project="recycle_trash_OD", name="universenet101_gfl_fp16_4x4_mstrain_480_960_2x_coco_Blur")),
+        dict(type="WandbLoggerHook", init_kwargs=dict(project="recycle_trash_OD", name="universenet101_gfl_fp16_4x4_mstrain_480_960_2x_coco_Blur_adam")),
     ],
 )
